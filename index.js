56b1932d51b13c11feffe51c54a50f4743ae0129
Victoria <95590803+anarayner@users.noreply.github.com>
2022-11-07 17:33:30 -0500

    fix

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
 server.use(jsonServer.defaults({}));
 server.use(jsonServer.bodyParser);
 
-// delay imitation
 server.use(async (req, res, next) => {
-    await new Promise((res) => {
-        setTimeout(res, 2000);
-    });
     next();
 });
 
@@ -29,7 +25,6 @@ server.get('/item', (req, res) => {
 
 server.use(router);
 
-// запуск сервера
 server.listen(8000, () => {
     console.log('server is running on 8000 port');
 });
